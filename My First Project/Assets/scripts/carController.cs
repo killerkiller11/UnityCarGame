551154249137a810f1310e1c36c6cabd599db321
killerkiller11 <balastudor@yahoo.com>
2016-02-28 15:43:50 +0200

    First Update
    Added Unity Project to GitHub

---
+++ My First Project/Assets/scripts/carController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carController : MonoBehaviour {
+
+    public float carSpeed;
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -2.2f , 2.2f);
+        transform.position = position;
+	}
+}
---
+++ My First Project/Assets/scripts/generator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generator : MonoBehaviour {
+
+    public GameObject enemy;
+    Vector3 carpos;
+    int time,delay,dif;
+    float poz,oldPoz;
+
+	// Use this for initialization
+	void Start () {
+        delay = 50;
+        dif = 0;
+        oldPoz = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = Time.frameCount;
+        if (dif < 24)
+        {
+            if (time % 50 == 0)
+                dif += 1;
+        }
+        if(time == delay)
+        {
+            if(oldPoz>=0)
+                poz = Random.Range (-2.2f,oldPoz);
+            else
+                poz = Random.Range(oldPoz, 2.2f);
+            oldPoz = poz;
+            delay = time + Random.Range(25-dif, 30-dif);
+            if(poz >= 0)
+            carpos = new Vector3(poz, transform.position.y, transform.position.z);
+            Instantiate(enemy, carpos, transform.rotation);
+        }
+
+	}
+}
